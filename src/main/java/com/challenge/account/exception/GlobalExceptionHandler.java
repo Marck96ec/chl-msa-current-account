4e23b9b5951584e38c9b4560c63846dd2a5fc40d
maclavij <maclavij@pichincha.com>
2024-12-16 23:43:58 -0500

    update fix error global

--- src/main/java/com/challenge/account/exception/GlobalExceptionHandler.java
+++ src/main/java/com/challenge/account/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.challenge.account.exception;
 
+import com.challenge.customer.server.models.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,8 +12,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<Error> handleIllegalArgumentException(IllegalArgumentException ex) {
-        Error errorResponse = new Error()
+    public ResponseEntity<com.challenge.customer.server.models.Error> handleIllegalArgumentException(IllegalArgumentException ex) {
+        com.challenge.customer.server.models.Error errorResponse = new com.challenge.customer.server.models.Error()
                 .title("Bad Request")
                 .detail(ex.getMessage())
                 .status(HttpStatus.BAD_REQUEST.value());
@@ -21,32 +22,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Error> handleGeneralException(Exception ex) {
-        Error errorResponse = new Error()
+    public ResponseEntity<com.challenge.customer.server.models.Error> handleGeneralException(Exception ex) {
+        com.challenge.customer.server.models.Error errorResponse = new Error()
                 .title("Internal Server Error")
                 .detail(ex.getMessage())
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
-
-class Error {
-    private String title;
-    private String detail;
-    private int status;
-
-    public Error title(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public Error detail(String detail) {
-        this.detail = detail;
-        return this;
-    }
-
-    public Error status(int status) {
-        this.status = status;
-        return this;
     }
 }
